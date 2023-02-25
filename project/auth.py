a658b5ca516b7f7a48ed9ce23dfca6e426e7beb0
freakazoidas <freakazoidas@gmail.com>
2023-02-25 17:45:22 +0200

    registration now requires 2 identical passwords and redirects to groups

--- project/auth.py
+++ project/auth.py
@@ -28,7 +28,8 @@ def login_post():
 
     # if the above check passes, then we know the user has the right credentials
     login_user(user, remember=remember)
-    return redirect(url_for('main.profile'))
+    return redirect(url_for('main.groups'))
+
 
 
 
@@ -37,20 +38,24 @@ def login_post():
 def signup():
     return render_template('signup.html')
 
+
 @auth.route('/signup', methods=['POST'])
 def signup_post():
     # code to validate and add user to database goes here
     email = request.form.get('email')
     name = request.form.get('name')
     password = request.form.get('password')
+    confirm_password = request.form.get('confirm_password')
 
     user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
     if user: # if a user is found, we want to redirect back to signup page so user can try again
         flash('Email address already exists')
         return redirect(url_for('auth.signup'))
-        
-    
-    
+
+    if password != confirm_password:
+        flash('Passwords do not match, please try again')
+        return redirect(url_for('auth.signup'))
+
     # create a new user with the form data. Hash the password so the plaintext version isn't saved.
     new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
 
